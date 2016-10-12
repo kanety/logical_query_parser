6cc64d89fe98c9045a7e9310bc22b1b8ce057475
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-10-12 10:57:17 +0900

    Update spec

--- spec/logical_query_parser/nodes/active_record_spec.rb
+++ spec/logical_query_parser/nodes/active_record_spec.rb
@@ -5,78 +5,66 @@ require 'logical_query_parser/nodes/active_record'
 describe 'LogicalQueryParser::ActiveRecord' do
   before do
     @parser = LogicalQueryParser.new
-    @opts = { model: Doc, columns: %w(c1 c2) }
+    @opts = { model: Doc, columns: %w(title body) }
   end
 
   it 'parses one word' do
     result = @parser.parse("aa").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses two words' do
     result = @parser.parse("aa bb").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses negative operation (1)' do
     result = @parser.parse("-aa bb").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses negative operation (2)' do
     result = @parser.parse("aa -bb").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses quoted string' do
     result = @parser.parse('"aa bb"').to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses AND operation' do
     result = @parser.parse("aa AND bb").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses AND operations' do
     result = @parser.parse("aa AND bb AND cc").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses AND operation with parentheses' do
     result = @parser.parse("(aa AND bb)").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses AND and OR operations (1)' do
     result = @parser.parse("(aa OR bb) AND cc").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses AND and OR operations (2)' do
     result = @parser.parse("aa AND (bb OR cc)").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses complex operations (1)' do
     result = @parser.parse("(aa OR bb) AND (cc OR dd)").to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 
   it 'parses complex operations (2)' do
     result = @parser.parse('"aa bb" AND -"cc dd" AND (ee OR ff)').to_sql(@opts)
-    puts result
-    expect(result).not_to be nil
+    expect(Doc.where(result).to_a).not_to be nil
   end
 end
--- spec/logical_query_parser/nodes/base_spec.rb
+++ spec/logical_query_parser/nodes/base_spec.rb
@@ -8,62 +8,62 @@ describe 'LogicalQueryParser::Base' do
 
   it 'parses one word' do
     result = @parser.parse("aa")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses two words' do
     result = @parser.parse("aa bb")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses negative operation (1)' do
     result = @parser.parse("-aa bb")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses negative operation (2)' do
     result = @parser.parse("aa -bb")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses quoted string' do
     result = @parser.parse('"aa bb"')
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses AND operation' do
     result = @parser.parse("aa AND bb")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses AND operations' do
     result = @parser.parse("aa AND bb AND cc")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses AND operation with parentheses' do
     result = @parser.parse("(aa AND bb)")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses AND and OR operations (1)' do
     result = @parser.parse("(aa OR bb) AND cc")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses AND and OR operations (2)' do
     result = @parser.parse("aa AND (bb OR cc)")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses complex operations (1)' do
     result = @parser.parse("(aa OR bb) AND (cc OR dd)")
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'parses complex operations (2)' do
     result = @parser.parse('"aa bb" AND -"cc dd" AND (ee OR ff)')
-    expect(result).not_to be nil
+    expect(result).to be_a_kind_of Treetop::Runtime::SyntaxNode
   end
 
   it 'returns nil if it cannot parse input string' do
